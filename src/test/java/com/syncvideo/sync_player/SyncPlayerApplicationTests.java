df6c0fb6ca784a3c7d4a4eec02f65d105a97c0ce
Sabyasachi Mohanty <147181258+Sabyasachi-4324@users.noreply.github.com>
2026-01-16 21:56:56 +0530

    Add files via upload

---
+++ src/test/java/com/syncvideo/sync_player/SyncPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncvideo.sync_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
